9d124803a9b7690a5a2cdf5afe7f38b168c6924d
Lewes Walton <lewes.walton@appsfactory.de>
2021-08-26 14:54:39 +0200

    homepage added

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod quis ullam
+      veritatis odio ipsa. Iusto reiciendis cupiditate eveniet, ex suscipit nemo
+      temporibus incidunt debitis quibusdam, recusandae ab facere ipsum numquam,
+      autem iure fugit at dolorum? Fugiat sint laborum consequuntur repudiandae!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  //   props: ["title"],
+  data() {
+    return {
+      title: "Home Page",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 20px 70px;
+}
+</style>
